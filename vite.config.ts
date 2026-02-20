7f76b60c9ac449a0fbbcaa9f74a9e9d7e9cc1a86
abdulrhmaanm <abdulrhmanmahmod77@icloud.com>
2026-02-20 15:50:55 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: "./",
+  base: process.env.VITE_BASE_PATH || "/StreamX",
 });
