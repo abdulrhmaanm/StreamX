f6ad02946ebcb968b5ee79d381d4e06f1555d858
abdulrhmaanm <abdulrhmanmahmod77@icloud.com>
2026-02-20 16:25:52 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import PopularOfWeek from "@/components/PopularOfWeek";
 import PopularShowsOfWeek from "@/components/PopularShowsofWeek";
 import  JustReleased  from "@/components/releaseCard";
 import DefaultLayout from "@/layouts/default";
-import { Navbar } from '@/components/navbar';
 
 
 export default function IndexPage() {
